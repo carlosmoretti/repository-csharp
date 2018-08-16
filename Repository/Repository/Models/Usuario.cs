1b63f72a3ad42227ed5f32caee617a03acd42333
Carlos Moretti <profissional.casm@gmail.com>
2018-08-15 22:50:04 -0300

    Alterações feitas na noite. Incompleto!

---
+++ Repository/Repository/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class Usuario
+    {
+        public string nome { get; set; }
+
+        public string sobrenome { get; set; }
+
+        public DateTime dataNascimento { get; set; }
+    }
+}
---
+++ Repository/Repository/Repositories/IRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public interface IRepositorio<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void AddAll(List<TEntity> entity);
+        void Edit(TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
